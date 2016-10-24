c48bd9e201c51c122e94f88bc085c6f56ae7ce88
vlad <inge__garcia@hotmail.com>
2016-10-24 14:02:39 -0500

    erores

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,10 @@
-configure do
-  enable :sessions
-end
-
-before "/users/:id" do
-  unless session[:id]
+enable :sessions
+before "/profile/:id" do
+  unless session[:id].inspect
     redirect to("/")
   end
 end
+
 #primera vista default
 get '/' do
   erb :index
@@ -14,9 +12,9 @@ end
 #se agrega el usuario a la db y lo redirige a su id unico
 post '/signup' do
   user = User.new(params[:user])
-  if user
-   user.id=  session[:user_id]
-    redirect to("/users/#{user.id}")
+  if user.save
+    session[:id].inspect = user.id
+    redirect to("/profile/#{user.id}")
   end
 end
 
@@ -24,21 +22,33 @@ get '/signup' do
   erb :signup
 end
 
+#despliega la vista bienvenido una vez logeado con el id
+
+
 #muestra el logeo de usuarios ya registrados
 post '/login' do
-  user = params[:user]
-  puts user
-  puts  user["email"]
-  puts  user["password"]
-
-  user = User.authenticate(user["email"], user["password"])
-  if user == nil
-    erb :error
+user =params[:user]
+user = User.authenticate(user["email"],user["password"])
+  #user = User.authenticate(user["em
+puts user
+  if user== nil
+    erb:error
   else
-erb :bienvenido
+    id = user.id
+    session[:id] = id
+    redirect to("/profile/#{id}" )
+
+end
+end
+before "/profile/:id" do
+  unless session[:id].inspect
+    redirect to("/")
   end
 end
 
+get '/profile/:id'do
+erb :bienvenido
+end
 #despliega la vista login.erb
 get '/login' do
   erb :login
